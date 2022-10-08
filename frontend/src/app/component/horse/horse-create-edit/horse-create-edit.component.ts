2547675b20acef103d17590e291982d20245104a
folsze <81622754+folsze@users.noreply.github.com>
2022-10-08 18:00:37 +0200

    [US1] FE: changed to reactive forms, improved validation also: disabled owner add temporarily

--- frontend/src/app/component/horse/horse-create-edit/horse-create-edit.component.ts
+++ frontend/src/app/component/horse/horse-create-edit/horse-create-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm, NgModel} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {Observable, of} from 'rxjs';
@@ -9,11 +9,10 @@ import {Sex} from 'src/app/dto/sex';
 import {HorseService} from 'src/app/service/horse.service';
 import {OwnerService} from 'src/app/service/owner.service';
 
-
 export enum HorseCreateEditMode {
   create,
   edit,
-};
+}
 
 @Component({
   selector: 'app-horse-create-edit',
@@ -22,14 +21,28 @@ export enum HorseCreateEditMode {
 })
 export class HorseCreateEditComponent implements OnInit {
 
-  mode: HorseCreateEditMode = HorseCreateEditMode.create;
-  horse: Horse = {
+  public mode: HorseCreateEditMode = HorseCreateEditMode.create;
+
+  public horseForm: FormGroup = this.formBuilder.group({
+    id: [null],
+    name: [null, [Validators.required, this.noWhitespaceInsideValidator]],
+    description: [null],
+    dateOfBirth: [null, [Validators.required, this.noDateInFutureValidator]],
+    sex: [null, [Validators.required]],
+    ownerFullName: [''],
+    owner: [null], // owner gets selected by using the ownerFullName control
+    motherName: [''],
+    mother: [null],
+    fatherName: [''],
+    father: [null]
+  });
+
+  public horse: Horse = {
     name: '',
     description: '',
     dateOfBirth: new Date(),
     sex: Sex.female,
   };
-
 
   constructor(
     private service: HorseService,
@@ -37,7 +50,47 @@ export class HorseCreateEditComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private notification: ToastrService,
-  ) {
+    private formBuilder: FormBuilder,
+  ) { }
+
+  public get name(): AbstractControl {
+    return this.horseForm.controls.name;
+  }
+
+  public get description(): AbstractControl {
+    return this.horseForm.controls.description;
+  }
+
+  public get dateOfBirth(): AbstractControl {
+    return this.horseForm.controls.dateOfBirth;
+  }
+
+  public get sex(): AbstractControl {
+    return this.horseForm.controls.sex;
+  }
+
+  public get ownerFullName(): AbstractControl {
+    return this.horseForm.controls.ownerFullName;
+  }
+
+  public get owner(): AbstractControl {
+    return this.horseForm.controls.owner;
+  }
+
+  public get motherName(): AbstractControl {
+    return this.horseForm.controls.motherName;
+  }
+
+  public get mother(): AbstractControl {
+    return this.horseForm.controls.mother;
+  }
+
+  public get fatherName(): AbstractControl {
+    return this.horseForm.controls.fatherName;
+  }
+
+  public get father(): AbstractControl {
+    return this.horseForm.controls.father;
   }
 
   public get heading(): string {
@@ -72,7 +125,18 @@ export class HorseCreateEditComponent implements OnInit {
     }
   }
 
-  ownerSuggestions = (input: string) => (input === '')
+  public noWhitespaceInsideValidator(control: AbstractControl) { // todo: private?
+    const containsWhitespace = (control.value || '').trim().match(/\s/g);
+    const isValid = !containsWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
+
+  public noDateInFutureValidator(dateControl: AbstractControl) {
+    const now: Date = new Date();
+    return dateControl.value < now ? null : { dateInFuture: true };
+  }
+
+  public ownerSuggestions = (input: string) => (input === '')
     ? of([])
     : this.ownerService.searchByName(input, 5);
 
@@ -82,12 +146,12 @@ export class HorseCreateEditComponent implements OnInit {
     });
   }
 
-  public dynamicCssClassesForInput(input: NgModel): any {
+  public dynamicCssClassesForInput(input: AbstractControl): any {
     return {
       // This names in this object are determined by the style library,
       // requiring it to follow TypeScript naming conventions does not make sense.
       // eslint-disable-next-line @typescript-eslint/naming-convention
-      'is-invalid': !input.valid && !input.pristine,
+      'is-invalid': input.invalid && input.dirty,
     };
   }
 
@@ -98,32 +162,33 @@ export class HorseCreateEditComponent implements OnInit {
   }
 
 
-  public onSubmit(form: NgForm): void {
-    console.log('is form valid?', form.valid, this.horse);
-    if (form.valid) {
-      if (this.horse.description === '') {
-        delete this.horse.description;
-      }
-      let observable: Observable<Horse>;
-      switch (this.mode) {
-        case HorseCreateEditMode.create:
-          observable = this.service.create(this.horse);
-          break;
-        default:
-          console.error('Unknown HorseCreateEditMode', this.mode);
-          return;
-      }
-      observable.subscribe({
-        next: data => {
-          this.notification.success(`Horse ${this.horse.name} successfully ${this.modeActionFinished}.`);
-          this.router.navigate(['/horses']);
-        },
-        error: error => {
-          console.error('Error creating horse', error);
-          // TODO show an error message to the user. Include and sensibly present the info from the backend!
-        }
-      });
-    }
-  }
+  public onSubmit(): void {
+    const sendHorse: Horse = {
+      name: this.name.value.trim(),
+      description: this.description.value?.trim(),
+      dateOfBirth: this.dateOfBirth.value,
+      sex: this.sex.value,
+      owner: this.owner?.value,
+    };
 
+    let horse$: Observable<Horse>;
+
+    if (this.mode === HorseCreateEditMode.create) {
+      horse$ = this.service.create(sendHorse);
+    } else {
+      console.error('Edit mode not implemented yet.');
+      return;
+    }
+
+    horse$.subscribe({
+      next: horse => {
+        this.notification.success(`Horse ${horse.name} successfully ${this.modeActionFinished}.`);
+        this.router.navigate(['/horses']);
+      },
+      error: error => {
+        console.error('Error creating horse', error);
+        // TODO show an error message to the user. Include and sensibly present the info from the backend!
+      }
+    });
+  }
 }
