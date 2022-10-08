38321973f7e82435a7841460e1d0664fae55b709
folsze <81622754+folsze@users.noreply.github.com>
2022-10-08 20:33:36 +0200

    [US8] FE: adjusted wordings

--- frontend/src/app/component/owner/owner.component.ts
+++ frontend/src/app/component/owner/owner.component.ts
@@ -31,12 +31,12 @@ export class OwnerComponent implements OnInit {
           this.owners = data;
         },
         error: error => {
-          console.error('Error fetching horses', error);
-          this.bannerError = 'Could not fetch horses: ' + error.message;
+          console.error('Error fetching owners', error);
+          this.bannerError = 'Could not fetch owners: ' + error.message;
           const errorMessage = error.status === 0
             ? 'Is the backend up?'
             : error.message.message;
-          this.notification.error(errorMessage, 'Could Not Fetch Horses');
+          this.notification.error(errorMessage, 'Could Not Fetch Owners');
         }
       });
   }
